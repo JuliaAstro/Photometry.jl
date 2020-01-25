9d00829d1654ba3993ac0f276f2ecd7ebf1c7fbb
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-25 16:56:05 +0000

    build based on f2b5ebc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#","page":"Home","title":"Home","text":"CurrentModule = AperturePhotometry","category":"page"},{"location":"index.html#AperturePhotometry-1","page":"Home","title":"AperturePhotometry","text":"","category":"section"},{"location":"index.html#","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html#","page":"Home","title":"Home","text":"Modules = [AperturePhotometry]","category":"page"}]
+}
