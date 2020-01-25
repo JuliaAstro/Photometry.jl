636d7ae286c6d4a691e94b71e78db3e5bb768ab8
Miles Lucas <mdlucas@hawaii.edu>
2020-01-25 07:42:44 -1000

    start working on masking techniques

--- src/AperturePhotometry.jl
+++ src/AperturePhotometry.jl
@@ -1,15 +1,40 @@
 module AperturePhotometry
 
-export area
+export area,
+       mask,
+       Exact,
+       Center,
+       Subpixel
 
 abstract type Aperture end
+
+abstract type OverlapMethod end
+struct Exact <: OverlapMethod end
+struct Center <: OverlapMethod end
+struct Subpixel <: OverlapMethod 
+    n::Integer
+end
 
 """
     area(::Aperture)
 
 Returns the geometric area of the aperture
 """
-area(::Aperture) = 0
+area(::Aperture)
+
+"""
+    mask(::Aperture)
+
+Return a `BitArray` of the masked aperture values in the minimum bounding box
+"""
+mask(::Aperture)
+
+"""
+    bbox(::Aperture)
+
+Return the (`xlow`, `xhigh`, `ylow`, `yhigh`) bounds for a given Aperture
+"""
+bbox(::Aperture)
 
 include("circular.jl")
 
--- src/circular.jl
+++ src/circular.jl
@@ -14,6 +14,20 @@ end
 
 area(c::CircularAperture) = π * c.r^2
 
+bbox(c::CircularAperture) = (-c.r, c.r, -c.r, c.r)
+
+function mask(c::CircularAperture)
+    box = bbox(c)
+    nx = ny = Int(2c.r)
+    out = BitArray(false, nx, ny)
+    xs = box[1]:box[2]
+    ys = hcat(box[3]:box[4])
+    out[sqrt.(xs.^2 .+ ys.^2) .< c.r] .= true
+    return out
+end
+
+#######################################################
+
 struct CircularAnnulus{T <: Number} <: Aperture
     center::Tuple{T,T}
     r_in::T
@@ -26,3 +40,14 @@ function Base.show(io::IO, c::CircularAnnulus)
 end
 
 area(c::CircularAnnulus) = π * (c.r_out^2 - c.r_in^2)
+bbox(c::CircularAnnulus) = (-c.r, c.r, -c.r, c.r)
+
+function mask(c::CircularAnnulus)
+    box = bbox(c)
+    nx = ny = Int(2c.r)
+    out = BitArray(false, nx, ny)
+    xs = box[1]:box[2]
+    ys = hcat(box[3]:box[4])
+    out[c.r_in .< sqrt.(xs.^2 .+ ys.^2) .< c.r_out] .= true
+    return out
+end
