18c839cd20421f4720dac533382eb5d4c974fbcb
Miles Lucas <mdlucas@hawaii.edu>
2020-02-26 15:40:42 -1000

    get tests working for Mean

--- test/background/simple.jl
+++ test/background/simple.jl
@@ -2,16 +2,16 @@ function test_ones(estimator)
     data = ones(10, 10)
 
     @test estimate_background(estimator, data) ≈ 1.0
-    @test estimate_background(estimator, data, dims = 0) ≈ ones(10)
     @test estimate_background(estimator, data, dims = 1) ≈ ones(1, 10)
+    @test estimate_background(estimator, data, dims = 2) ≈ ones(10)
 end
 
 function test_zeros(estimator)
     data = zeros(10, 10)
 
     @test estimate_background(estimator, data) ≈ 0.0
-    @test estimate_background(estimator, data, dims = 0) ≈ zeros(10)
     @test estimate_background(estimator, data, dims = 1) ≈ zeros(1, 10)
+    @test estimate_background(estimator, data, dims = 2) ≈ zeros(10)
 end
 
 @testset "$E"  for E in [Mean]
