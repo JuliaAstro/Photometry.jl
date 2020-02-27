fa5e2d61da53724512e4659e980431f229669cc3
Miles Lucas <mdlucas@hawaii.edu>
2020-02-26 15:49:09 -1000

    make sure mesh fitting is broken

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,5 +10,7 @@ using Test
 end
 
 @testset "Background Estimation" begin
+    # mesh fitting not supported yet
+    @test_throws ErrorException estimate_background(Mean, ones(1, 1), (1, 1), (1, 1))
     include("background/simple.jl")
 end
