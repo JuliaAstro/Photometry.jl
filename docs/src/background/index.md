e1819af72cfd843d8970026c7d62ae055ca86a52
Siddharth <siddharthlal25@gmail.com>
2020-03-01 02:33:03 +0530

    minor fix

--- docs/src/background/index.md
+++ docs/src/background/index.md
@@ -1,9 +1,10 @@
 # Background Estimation
 
-The module provides tools and algorithms for estimating the background of astronomical data. 
+The module provides tools and algorithms for estimating the background of astronomical data.
 
 ## API/Reference
 
 ```@docs
 estimate_background
+sigma_clip
 ```
--- test/background/stat_estimators.jl
+++ test/background/stat_estimators.jl
@@ -1,11 +1,10 @@
 using Photometry.Background: sigma_clip
 
 @testset "sigma clipping" begin
-    x = [1.0, 2.0, 3.0]
+    x = [1, 2, 3]
     @test sigma_clip(x, 1, 1) ≈ [1.0, 2.0, 3.0] rtol=1e-4
     @test sigma_clip(x, 1) ≈ [1.0, 2.0, 3.0] rtol=1e-4
 
-    y = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-    @test sigma_clip(y,1,1,iterations=20) ≈ [2.77002, 2.77002, 3.0, 4.0, 4.22998, 4.22998] rtol=1e-4
-    @test sigma_clip(y,1,1) ≈ [2.30234, 2.30234, 3.0, 4.0, 4.69766, 4.69766] rtol=1e-4
+    y = [1, 2, 3, 4, 5, 6]
+    @test sigma_clip(y,1,1) ≈ [1.62917, 2.0, 3.0, 4.0, 5.0, 5.37083] rtol=1e-4
 end
