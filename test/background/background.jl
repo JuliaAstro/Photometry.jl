c45d730f2df9a268f6e0b86a313edcb4b87fe9fc
Miles Lucas <mdlucas@hawaii.edu>
2020-03-14 01:34:26 -1000

    tests for zoom interpolator

--- test/background/background.jl
+++ test/background/background.jl
@@ -7,17 +7,21 @@
     y = [1, 2, 3, 4, 5, 6]
     @test sigma_clip(y, 1, 1) ≈ [1.62917, 2.0, 3.0, 4.0, 5.0, 5.37083] rtol = 1e-4
 
-# using different center
+    # using different center
     @test sigma_clip(y, 1, center = 4) ≈ [2.1291713066130296, 2.1291713066130296, 3, 4, 5, 5.87082869338697]
-# using different std
+    # using different std
     @test sigma_clip(y, 1, std = 1) ≈ [2.5, 2.5, 3, 4, 4.5, 4.5]
 end
 
-@testset "functional stuff" begin
+@testset "estimate_background interface" begin
     data = ones(100, 100)
 
-    @test_throws ErrorException estimate_background(data, (4, 4), edge_method = :yeet)
     @test all(estimate_background(data, 20) .== estimate_background(data, (20, 20)))
+    @test size(estimate_background(data, 19, edge_method = :pad)[1]) == (114, 114)
+    @test size(estimate_background(data, 19, edge_method = :crop)[1]) == (95, 95)
+
+    @test_throws ErrorException estimate_background(data, (4, 4), edge_method = :yeet)
+    @test_throws MethodError estimate_background(data, (4, 4, 4))
 end
 
 @testset "flat background - $B, $S" for B in [Mean, Median, MMM, BiweightLocation, SourceExtractor], S in [StdRMS, MADStdRMS, BiweightScaleRMS]
@@ -30,4 +34,15 @@ end
     bk, rms = estimate_background(data, (25, 25), B(), S())
     @test median(bk) ≈ 1
     @test median(rms) ≈ 0
+end
+
+@testset "interpolators" begin
+
+    @testset "zoom interface" begin
+        @test ZoomInterpolator(3) == ZoomInterpolator(3, 3) == ZoomInterpolator((3, 3))
+
+        z = ZoomInterpolator(4, 3)
+        @test z(ones(3, 3)) ≈ ones(12, 9)
+    end
+
 end
