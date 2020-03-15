7cc058a26b375c0c26efa86089d949763e1fd1d6
Miles Lucas <mdlucas@hawaii.edu>
2020-03-15 03:27:15 -1000

    add license notes

--- src/Background/estimators.jl
+++ src/Background/estimators.jl
@@ -1,3 +1,7 @@
+#= 
+Part of this work is derived from astropy/photutils and astropy/astropy. The relevant derivations
+are considered under a BSD 3-clause license. =#
+
 using Statistics
 using StatsBase
 
