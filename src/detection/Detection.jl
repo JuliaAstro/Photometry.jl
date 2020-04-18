d7fd8f63ceb1b587fb27e3b366cbb74bbf200350
Miles Lucas <mdlucas@hawaii.edu>
2020-04-17 19:25:04 -1000

    fix two typos

--- src/detection/Detection.jl
+++ src/detection/Detection.jl
@@ -34,7 +34,7 @@ extract_sources
 
 Detect sources by finding peaks above a threshold in grids across the image.
 
-This creates a pixel-wise threshold for sources by calculating `berror * nsigma` when used with [`extract_sources`](@ref)
+This creates a pixel-wise threshold for sources by calculating `error * nsigma` when used with [`extract_sources`](@ref)
 The peaks are found by searching the image in boxes of size `box_size`. If the maximum value in that box is greater than the threshold set above, the point is extracted.
 """
 @with_kw struct PeakMesh <: SourceFinder
