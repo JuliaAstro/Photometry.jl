8503d0273f15aa84c4ca392e0c8834e3a402e0ef
Miles Lucas <mdlucas@hawaii.edu>
2020-11-11 13:23:21 -0600

    tidy up plotting and add vector of aps recipe

--- src/aperture/plotting.jl
+++ src/aperture/plotting.jl
@@ -1,135 +1,144 @@
 using RecipesBase
 
-@recipe function f(c::CircularAperture, npoints = 1000)
-    seriestype := :path
-    aspect_ratio --> :equal
-    seriescolor --> :red
-    label --> ""
-    t = range(0, 2pi, length = npoints)
-
-    x = @. c.x + c.r * sin(t) + 0.5
-    y = @. c.y + c.r * cos(t) + 0.5
-
-
-    x, y
+function circle(x, y, r, θ)
+    u = x + r * cos(θ) + 0.5
+    v = y + r * sin(θ) + 0.5
+    return u, v
 end
 
-@recipe function f(c::CircularAnnulus, npoints = 1000)
-    t = range(0, 2pi, length = npoints)
-    seriestype := :path
-    seriescolor --> :red
+circle(ap::CircularAperture, θ) = circle(ap.x, ap.y, ap.r, θ)
+
+function ellipse(x, y, a, b, θ, t=0)
+    u = x + a * cos(θ) * cosd(t) - b * sin(θ) * sind(t) + 0.5
+    v = y + a * cos(θ) * sind(t) + b * sin(θ) * cosd(t) + 0.5
+    return u, v
+end
+
+ellipse(ap::EllipticalAperture, θ) = ellipse(ap.x, ap.y, ap.a, ap.b, θ, ap.theta)
+
+@recipe function f(c::CircularAperture, npoints = 100)
+    seriestype := :shape
     aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
     label --> ""
 
+    t = range(0, 2pi, length = npoints)
+    circle.((c,), t)
+end
+
+@recipe function f(c::CircularAnnulus, npoints = 100)
+    seriestype := :shape
+    aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
+    label --> ""
+
+
+    t = range(0, 2pi, length = npoints)
     # outer ring
     @series begin
-        x = @. c.x + c.r_out * cos(t) + 0.5
-        y = @. c.y + c.r_out * sin(t) + 0.5
-        x, y
+        circle.(c.x, c.y, c.r_out, t)
     end
 
     # inner ring
     @series begin
-        x = @. c.x + c.r_in * cos(t) + 0.5
-        y = @. c.y + c.r_in * sin(t) + 0.5
-        x, y
+        circle.(c.x, c.y, c.r_in, t)
     end
-
-
 end
 
 
-@recipe function f(e::EllipticalAperture, npoints = 1000)
-    seriestype := :path
-    seriescolor --> :red
+@recipe function f(e::EllipticalAperture, npoints = 100)
+    seriestype := :shape
     aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
     label --> ""
 
     t = range(0, 2pi, length = npoints)
-    x = @. e.x + e.a * cos(t) * cosd(e.theta) - e.b * sin(t) * sind(e.theta) + 0.5
-    y = @. e.y + e.a * cos(t) * sind(e.theta) + e.b * sin(t) * cosd(e.theta) + 0.5
-
-    x, y
+    ellipse.((e,), t)
 end
 
-@recipe function f(e::EllipticalAnnulus, npoints = 1000)
-    t = range(0, 2pi, length = npoints)
-    seriestype := :path
-    seriescolor --> :red
+@recipe function f(e::EllipticalAnnulus, npoints = 100)
+    seriestype := :shape
     aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
     label --> ""
+
+    t = range(0, 2pi, length = npoints)
 
     # outer ring
     @series begin
-        x = @. e.x + e.a_out * cos(t) * cosd(e.theta) - e.b_out * sin(t) * sind(e.theta) + 0.5
-        y = @. e.y + e.a_out * cos(t) * sind(e.theta) + e.b_out * sin(t) * cosd(e.theta) + 0.5
-        x, y
+        ellipse.(e.x, e.y, e.a_out, e.b_out, t, e.theta)
     end
 
     # inner ring
     @series begin
-        x = @. e.x + e.a_in * cos(t) * cosd(e.theta) - e.b_in * sin(t) * sind(e.theta) + 0.5
-        y = @. e.y + e.a_in * cos(t) * sind(e.theta) + e.b_in * sin(t) * cosd(e.theta) + 0.5
-        x, y
+        ellipse.(e.x, e.y, e.a_in, e.b_in, t, e.theta)
     end
 end
 
+function rectangle(x, y, w, h, θ, t=0)
+    half_w, half_h = w / 2, h / 2
+    sinth, costh = sincos(θ)
+    u = half_w * (abs(costh) * costh + abs(sinth) * sinth)
+    v = half_h * (abs(costh) * costh - abs(sinth) * sinth)
+    sint, cost = sincos(deg2rad(t))
+    j = x + u * cost - v * sint + 0.5
+    k = y + u * sint + v * cost + 0.5
+    return j, k
+end
 
-@recipe function f(ap::RectangularAperture, npoints = 1000)
-    seriestype := :path
-    seriescolor --> :red
+rectangle(ap::RectangularAperture, θ) = rectangle(ap.x, ap.y, ap.w, ap.h, θ, ap.theta)
+
+
+@recipe function f(ap::RectangularAperture, npoints = 100)
+    seriestype := :shape
     aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
     label --> ""
 
     t = range(0, 2π, length = npoints)
-    w2 = ap.w / 2
-    h2 = ap.h / 2
-
-    x = @. w2 * (abs(cos(t)) * cos(t) + abs(sin(t)) * sin(t))
-    y = @. h2 * (abs(cos(t)) * cos(t) - abs(sin(t)) * sin(t))
-
-    sinth, costh = sincos(deg2rad(ap.theta))
-    u = @. ap.x + x * costh - y * sinth + 0.5
-    v = @. ap.y + x * sinth + y * costh + 0.5
-
-    u, v
+    return rectangle.((ap,), t)
 end
 
-@recipe function f(ap::RectangularAnnulus, npoints = 1000)
-    seriestype := :path
-    seriescolor --> :red
+@recipe function f(ap::RectangularAnnulus, npoints = 100)
+    seriestype := :shape
     aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
     label --> ""
 
     t = range(0, 2π, length = npoints)
 
     # outer box
     @series begin
-        w2 = ap.w_out / 2
-        h2 = ap.h_out / 2
-
-        x = @. w2 * (abs(cos(t)) * cos(t) + abs(sin(t)) * sin(t))
-        y = @. h2 * (abs(cos(t)) * cos(t) - abs(sin(t)) * sin(t))
-
-        sinth, costh = sincos(deg2rad(ap.theta))
-        u = @. ap.x + x * costh - y * sinth + 0.5
-        v = @. ap.y + x * sinth + y * costh + 0.5
-
-        u, v
+        rectangle.(ap.x, ap.y, ap.w_out, ap.h_out, t, ap.theta)
     end
 
     # inner box
     @series begin
-        w2 = ap.w_in / 2
-        h2 = ap.h_in / 2
+        rectangle.(ap.x, ap.y, ap.w_in, ap.h_in, t, ap.theta)
+    end
+end
 
-        x = @. w2 * (abs(cos(t)) * cos(t) + abs(sin(t)) * sin(t))
-        y = @. h2 * (abs(cos(t)) * cos(t) - abs(sin(t)) * sin(t))
-
-        sinth, costh = sincos(deg2rad(ap.theta))
-        u = @. ap.x + x * costh - y * sinth + 0.5
-        v = @. ap.y + x * sinth + y * costh + 0.5
-
-        u, v
+@recipe function f(aps::AbstractVector{<:AbstractAperture})
+    seriestype := :shape
+    aspect_ratio --> :equal
+    seriescolor --> :red
+    linecolor --> :match
+    fillcolor --> nothing
+    label --> ""
+    
+    for ap in aps
+        @series begin ap end
     end
 end
