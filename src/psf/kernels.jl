5d3830fa7e842158b3f435570089bc64a62a5c22
Miles Lucas <mdlucas@hawaii.edu>
2020-11-29 02:42:41 -0600

    add preliminary moffat and airydisk kernels

--- src/psf/kernels.jl
+++ src/psf/kernels.jl
@@ -13,6 +13,25 @@ A kernel used for defining a PSF.
 """
 abstract type PSFKernel{T} <: AbstractMatrix{T} end
 
+Base.checkbounds(::Type{Bool}, ::PSFKernel, idx...) = true
+Base.checkbounds(::Type{Bool}, ::PSFKernel, idx::CartesianIndex) = true
+
+
+function indices_from_extent(pos, fwhm, maxsize)
+    halfextent = @. maxsize * fwhm / 2
+    lower = @. floor(Int, pos - halfextent)
+    upper = @. ceil(Int, pos + halfextent)
+    return (first(lower):first(upper), last(lower):last(upper))
+end
+
+
+# TODO
+# function indices_from_extent(pos, fwhm::AbstractMatrix, maxsize)
+#     halfextent = maxsize .* fwhm ./ 2
+#     lower = @. floor(Int, pos - halfextent)
+#     upper = @. ceil(Int, pos - halfextent)
+# end
+
 """
     Kernels.Gaussian(fwhm; maxsize=ceil(Int, 3fwhm))
     Kernels.Normal(fwhm; maxsize=ceil(Int, 3fwhm))
@@ -65,23 +84,8 @@ Gaussian{T}(xy::Tuple, fwhm; kwargs...) where {T} = Gaussian{T}(SVector(xy), fwh
 # # translate polar coordinates to cartesian, optionally recentering
 Gaussian{T}(p::Polar, fwhm; origin=SA[0, 0], kwargs...) where {T} = Gaussian(CartesianFromPolar()(p) .+ origin, fwhm; kwargs...)
 
-function indices_from_extent(pos, fwhm, maxsize)
-    halfextent = @. maxsize * fwhm / 2
-    lower = @. floor(Int, pos - halfextent)
-    upper = @. ceil(Int, pos + halfextent)
-    return (first(lower):first(upper), last(lower):last(upper))
-end
-
-# TODO
-# function indices_from_extent(pos, fwhm::AbstractMatrix, maxsize)
-#     halfextent = maxsize .* fwhm ./ 2
-#     lower = @. floor(Int, pos - halfextent)
-#     upper = @. ceil(Int, pos - halfextent)
-# end
-
 Base.size(g::Gaussian) = map(length, g.indices)
 Base.axes(g::Gaussian) = g.indices
-Base.checkbounds(::Type{Bool}, ::PSFKernel, idx...) = true
 
 # fallback, also covers scalar case
 function Base.getindex(g::Gaussian{T}, idx::Vararg{<:Integer,2}) where T
@@ -110,39 +114,97 @@ const Normal = Gaussian
 @doc (@doc Gaussian) Normal
 
 
-# """
-#     Kernels.Moffat(fwhm)
+############
 
-# A Moffat kernel
+"""
+    Kernels.AiryDisk(pos, fwhm)
+"""
+struct AiryDisk{T,FT,VT<:AbstractVector,IT<:Tuple} <: PSFKernel{T}
+    pos::VT
+    fwhm::FT
+    indices::IT
 
-# ```math
-# K(d) = \\left[1 + \\left(\\frac{d}{\\Gamma/2}\\right)^2 \\right]^{-1}
-# ```
+    AiryDisk{T}(pos::VT, fwhm::FT, indices::IT) where {T,VT<:AbstractVector,FT,IT<:Tuple} = new{T,FT,VT,IT}(pos, fwhm, indices)
+end
 
-# ```
-#   ┌────────────────────────────────────────┐
-# 1 │                  .::.                  │ Moffat(x)
-#   │                  : ::                  │
-#   │                 :' :':                 │
-#   │                 :  : :.                │
-#   │                :   :  :                │
-#   │               .:   :  :.               │
-#   │               :    :   :               │
-#   │              .:    :   '.              │
-#   │              :     :    '.             │
-#   │             :      :     :.            │
-#   │            :       :      :.           │
-#   │          .'        :       ':          │
-#   │       ..''         :         ':.       │
-#   │ ....:''            :            ''.... │
-# 0 │''                  :                  '│
-#   └────────────────────────────────────────┘
-#   -2fwhm                               2fwhm
-# ```
-# """
-# struct Moffat{T} <: PSFKernel
-#     fwhm::T
-# end
+## constructors
+# default type is Float64
+AiryDisk(args...; kwargs...) = AiryDisk{Float64}(args...; kwargs...)
+# parse indices from maxsize
+AiryDisk{T}(pos::AbstractVector, fwhm; maxsize=3) where {T} = AiryDisk{T}(pos, fwhm, indices_from_extent(pos, fwhm, maxsize))
+# default position is [0, 0]
+AiryDisk{T}(fwhm; kwargs...) where {T} = AiryDisk{T}(SA[0, 0], fwhm; kwargs...)
+# # parse position to vector
+AiryDisk{T}(x::Number, y::Number, fwhm; kwargs...) where {T} = AiryDisk{T}(SA[x, y], fwhm; kwargs...)
+AiryDisk{T}(xy::Tuple, fwhm; kwargs...) where {T} = AiryDisk{T}(SVector(xy), fwhm; kwargs...)
+# # translate polar coordinates to cartesian, optionally recentering
+AiryDisk{T}(p::Polar, fwhm; origin=SA[0, 0], kwargs...) where {T} = AiryDisk(CartesianFromPolar()(p) .+ origin, fwhm; kwargs...)
+
+Base.size(a::AiryDisk) = map(length, a.indices)
+Base.axes(a::AiryDisk) = a.indices
+
+const rz = 3.8317059702075125 / π
+
+function Base.getindex(a::AiryDisk{T}, idx::Vararg{<:Integer,2}) where T
+    radius = a.fwhm * 1.18677
+    Δ = euclidean(SVector(idx), a.pos)
+    r = Δ / (radius / rz)
+    val = ifelse(iszero(r), one(T), 2 * besselj1(π * r) / (π * r))
+    return convert(T, val)
+end
+
+function Base.getindex(a::AiryDisk{T,<:Union{AbstractVector,Tuple}}, idx::Vararg{<:Integer,2}) where T
+    weights = @. (rz / (a.fwhm * 1.18677))^2
+    r = weuclidean(SVector(idx), a.pos, SVector(weights...))
+    val = ifelse(iszero(r), one(T), 2 * besselj1(π * r) / (π * r))
+    return convert(T, val)
+end
+
+############
+
+"""
+    Kernels.Moffat(pos, fwhm)
+"""
+struct Moffat{T,FT,VT<:AbstractVector,IT<:Tuple} <: PSFKernel{T}
+    pos::VT
+    fwhm::FT
+    indices::IT
+
+    Moffat{T}(pos::VT, fwhm::FT, indices::IT) where {T,VT<:AbstractVector,FT,IT<:Tuple} = new{T,FT,VT,IT}(pos, fwhm, indices)
+end
+
+## constructors
+# default type is Float64
+Moffat(args...; kwargs...) = Moffat{Float64}(args...; kwargs...)
+# parse indices from maxsize
+Moffat{T}(pos::AbstractVector, fwhm; maxsize=3) where {T} = Moffat{T}(pos, fwhm, indices_from_extent(pos, fwhm, maxsize))
+# default position is [0, 0]
+Moffat{T}(fwhm; kwargs...) where {T} = Moffat{T}(SA[0, 0], fwhm; kwargs...)
+# # parse position to vector
+Moffat{T}(x::Number, y::Number, fwhm; kwargs...) where {T} = Moffat{T}(SA[x, y], fwhm; kwargs...)
+Moffat{T}(xy::Tuple, fwhm; kwargs...) where {T} = Moffat{T}(SVector(xy), fwhm; kwargs...)
+# # translate polar coordinates to cartesian, optionally recentering
+Moffat{T}(p::Polar, fwhm; origin=SA[0, 0], kwargs...) where {T} = Moffat(CartesianFromPolar()(p) .+ origin, fwhm; kwargs...)
+
+Base.size(m::Moffat) = map(length, m.indices)
+Base.axes(m::Moffat) = m.indices
+
+# scalar case
+function Base.getindex(m::Moffat{T}, idx::Vararg{<:Integer,2}) where T
+    hwhm = m.fwhm / 2
+    Δ = sqeuclidean(SVector(idx), a.pos)
+    val = inv(1 + Δ / hwhm^2)
+    return convert(T, val)
+end
+
+# vector case
+function Base.getindex(m::Moffat{T,<:Union{AbstractVector,Tuple}}, idx::Vararg{<:Integer,2}) where T
+    weights = @. (2 / m.fwhm)^2
+    Δ = wsqeuclidean(SVector(idx), a.pos, SVector(weights...))
+    val = inv(1 + Δ)
+    return convert(T, val)
+end
+
 
 # function (m::Moffat)(dist)
 #     hwhm = m.fwhm / 2
@@ -150,45 +212,4 @@ const Normal = Gaussian
 # end
 
 # """
-#     Kernels.AiryDisk(fwhm)
-
-# An Airy disk. Guaranteed to work even at `r=0`.
-
-# ```math
-# K(r) = \\left[\\frac{2J_1\\left(\\pi r \\right)}{\\pi r}\\right]^2 \\quad r \\approx \\frac{d}{0.97\\Gamma}
-# ```
-
-# ```
-#   ┌────────────────────────────────────────┐
-# 1 │                  .'::                  │ AiryDisk(x)
-#   │                 .' :':                 │
-#   │                 :  : :                 │
-#   │                :'  : ':                │
-#   │                :   :  :                │
-#   │               .'   :  ':               │
-#   │               :    :   :               │
-#   │              .'    :   ':              │
-#   │              :     :    :              │
-#   │             .:     :    '.             │
-#   │             :      :     :             │
-#   │            .'      :     ':            │
-#   │            :       :      :.           │
-#   │           :        :       :           │
-# 0 │..........:'        :        '..........│
-#   └────────────────────────────────────────┘
-#   -2fwhm                               2fwhm
-# ```
-# """
-# struct AiryDisk{T} <: PSFKernel
-#     fwhm::T
-# end
-
-# const rz = 3.8317059702075125 / π
-
-# function (a::AiryDisk)(dist)
-#     radius = a.fwhm * 1.18677
-#     r = dist / (radius / rz)
-#     return iszero(r) ? 1.0 : (2besselj1(π * r) / (π * r))^2
-# end
-
 end # module Kernels
