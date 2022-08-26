a8a699a99ad2a11953328f4b9dfc34a150dbc512
William Thompson <will.thompson@outlook.com>
2022-08-26 13:11:11 -0700

    Towards unified JuliaAstro docs

---
+++ docs/pages.jl
@@ -0,0 +1,19 @@
+pages = [
+    "Home" => "index.md",
+    "Background Estimation" => [
+        "Getting Started" => "background/index.md",
+        "Background Estimators" => "background/estimators.md",
+        "Background Interpolators" => "background/interpolators.md"
+    ],
+    "Source Detection" => [
+        "Getting Started" => "detection/index.md",
+        "Algorithms" => "detection/algs.md",
+    ],
+    "Aperture Photometry" => [
+        "Getting Started" => "apertures/index.md",
+        "Apertures" => "apertures/apertures.md",
+        "Examples" => "apertures/examples.md"
+    ]
+]
+
+requiredmods = Symbol[]
