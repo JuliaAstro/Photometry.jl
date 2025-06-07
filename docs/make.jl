890e9dfa80cfdf5ae160b954c253473903f1a345
Ian Weaver <weaveric@gmail.com>
2025-06-06 18:45:01 -0700

    style

--- docs/make.jl
+++ docs/make.jl
@@ -7,22 +7,24 @@ setup = quote
     using Random
     Random.seed!(123456)
 end
+
 DocMeta.setdocmeta!(Photometry, :DocTestSetup, setup; recursive = true)
 
 include("pages.jl")
-makedocs(
+
+makedocs(;
     modules = [Photometry],
     authors = "Miles Lucas <mdlucas@hawaii.edu>",
     repo = GitHub("JuliaAstro/Photometry.jl"),
     sitename = "Photometry.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://juliaastro.org/Photometry/stable",
+        canonical = "https://juliaastro.org/Photometry/stable/",
     ),
     pages = pages,
 )
 
-deploydocs(
+deploydocs(;
     repo = "github.com/JuliaAstro/Photometry.jl",
     push_preview = true,
     versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
