b2dd7688db19fad69e2b3da116772fe2876277db
icweaver <weaveric@gmail.com>
2025-08-04 22:51:00 -0700

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,22 +6,3 @@ using TestItemRunner
 Random.seed!(8462852)
 
 @run_package_tests
-
-#@testset "Aperture Photometry" begin
-#    include("aperture/overlap.jl")
-#    include("aperture/circular.jl")
-#    include("aperture/photometry.jl")
-#    include("aperture/elliptical.jl")
-#    include("aperture/rectangular.jl")
-#    include("aperture/plots.jl")
-#end
-
-#@testset "Background Estimation" begin
-#    include("background/background.jl")
-#    include("background/estimators.jl")
-#    include("background/interpolators.jl")
-#end
-#
-#@testset "Source Detection" begin
-#    include("detection/detection.jl")
-#end
