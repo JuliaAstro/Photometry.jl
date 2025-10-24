d5240ea12385bb39a23d1847b16c4d815b84d3d8
icweaver <weaveric@gmail.com>
2025-10-24 03:25:09 -0700

    docs up

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,10 +1,5 @@
 pages = [
     "Home" => "index.md",
-    "Background Estimation" => [
-        "Getting Started" => "background/index.md",
-        "Background Estimators" => "background/estimators.md",
-        "Background Interpolators" => "background/interpolators.md"
-    ],
     "Source Detection" => [
         "Getting Started" => "detection/index.md",
         "Algorithms" => "detection/algs.md",
