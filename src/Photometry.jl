8afad92b3f14368500b478aa2e0bd09232994736
icweaver <weaveric@gmail.com>
2025-10-24 00:32:50 -0700

    start moving out background module

--- src/Photometry.jl
+++ src/Photometry.jl
@@ -4,11 +4,10 @@ using TestItems
 using Reexport
 
 include("aperture/Aperture.jl")
-include("background/Background.jl")
 include("detection/Detection.jl")
 
 @reexport using .Aperture
-@reexport using .Background
+@reexport using BackgroundMeshes
 @reexport using .Detection
 
 end
