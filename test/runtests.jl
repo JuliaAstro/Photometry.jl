e797e0b2f0148181e1cfac93e1132671dec679a9
icweaver <weaveric@gmail.com>
2025-10-24 01:12:41 -0700

    make imports explicit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,10 @@
-using Photometry
-using Random
-using Statistics
-using ParallelTestRunner
+using ParallelTestRunner: runtests
+import Photometry
 
 const init_code = quote
     import StatsBase: median, mean, std, mad
 
     const DATA_DIR = joinpath(@__DIR__, "data")
 end
-
 
 runtests(Photometry, Base.ARGS; init_code)
